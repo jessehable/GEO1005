762d60eba4c4d3523191123f77bd3510b2bf545d
vetterelias <vetterelias@gmail.com>
2018-01-15 15:41:54 +0100

    Revert "changes"
    This reverts commit d4e07f5802e3751dc1452eea363b24c6a676e4fc.

--- Suburbia/MyPlugin/suburbia_search_tool_dockwidget.py
+++ Suburbia/MyPlugin/suburbia_search_tool_dockwidget.py
@@ -195,20 +195,13 @@ class MyPluginDockWidget(QtGui.QDockWidget, FORM_CLASS):
             self.iface.legendInterface().refreshLayerSymbology(layer_ui)
             self.canvas.refresh()
 
-    def showresults(self, feature):
-
-        if feature[18]:
-            percentage = (((feature[14] / ((feature[9] + feature[8]) / 2)) + (
-                    feature[15] / (((1 - feature[11]) + (1 - feature[12]) / 2))) + (feature[16] / (1 - feature[13])) + (
-                                   feature[17] / (1 - feature[10]))) / 100)
-            progres = (feature[18] / percentage)
-            self.progressBar.setValue(progres)
-
+    def showresults(self,feature):
+        self.progressBar.setValue(int(feature[18]))
         self.DisplayNeighborhoodName.setText(str(feature[1]))
-        self.ValuePeople.setNum(feature[8] * 100)
-        self.ValueChild.setNum((feature[6] * 1000))
-        self.ValueAccess.setNum((feature[7] * 1000))
-        self.ValueAfford.setNum((feature[4] * 1000))
+        self.ValuePeople.setNum(int(feature[8]))
+        self.ValueChild.setNum(int(feature[6]))
+        self.ValueAccess.setNum(int(feature[7]))
+        self.ValueAfford.setNum(int(feature[4]))
 
 #######
 #    Analysis functions
@@ -223,36 +216,13 @@ class MyPluginDockWidget(QtGui.QDockWidget, FORM_CLASS):
         else:
             self.ButtonConfirm.setEnabled(False)
 
+
     def Confirm(self):
         self.TabTerms.setEnabled(False)
         self.TabPreferences.setEnabled(True)
+        # self.SaveUserPreferences
         self.Tabs.setCurrentIndex(1)
-        # Whenever user confirms, the header off the later exported savefavorites csv is added.
-        header = ['Neighborhood',
-                  'Similar People (%)',
-                  'average distance to child care (m)',
-                  'average distance to trainstation (m)',
-                  'average housing price (euro)']
-        self.userdata.append(header)
 
-        # name = self.FieldName.text()
-        # age = self.FieldAge.text()
-        # gender = self.FieldGender.currentText()
-        # education = self.FieldEducation.currentText()
-        # new_row = [name,age,gender,education]
-        # self.userdata.append(new_row)
-
-    # def SaveUserInfo(self):
-    # open csv file for writing
-    # writer = csv.writer(open(unicode(path_csv)))
-    # new_row=[]
-    # new_row.append(self.FieldName.text())
-    # ew_row.append(self.FieldAge.text())
-    # writer.writerow(new_row)
-
-    # file = open(unicode(path),'w')
-    # file.write(age)
-    # file.close()
 
     def Explore(self):
 
@@ -266,41 +236,25 @@ class MyPluginDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
         layer_explore = uf.getLegendLayerByName(self.iface, "Rotterdam_Selection")
 
-        uf.updateField(layer_explore, 'B1', self.SliderPeople.value())
+        uf.updateField(layer_explore,'B1', self.SliderPeople.value())
         uf.updateField(layer_explore, 'B2', self.SliderChild.value())
         uf.updateField(layer_explore, 'B3', self.SliderAccess.value())
         uf.updateField(layer_explore, 'B4', self.SliderAfford.value())
 
         self.determineScore(layer_explore)
 
-        self.displayContinuousStyle(layer_explore, 'Score')
+        self.displayContinuousStyle(layer_explore,'Score')
+
 
     def Locate(self):
-
-        self.pref[0] = self.SliderPeople.value()
-        self.pref[1] = self.SliderChild.value()
-        self.pref[2] = self.SliderAccess.value()
-        self.pref[3] = self.SliderAfford.value()
-        self.TabPreferences.setEnabled(False)
-        self.TabMetrics.setEnabled(True)
-        self.Tabs.setCurrentIndex(2)
-
-        subburbe = self.spinBox.value()
-
-        layer_explore = uf.getLegendLayerByName(self.iface, "Rotterdam_Selection")
-
-        uf.updateField(layer_explore, 'B1', self.SliderPeople.value())
-        uf.updateField(layer_explore, 'B2', self.SliderChild.value())
-        uf.updateField(layer_explore, 'B3', self.SliderAccess.value())
-        uf.updateField(layer_explore, 'B4', self.SliderAfford.value())
-
-        self.determineScore(layer_explore)
-        uf.getFieldNames(layer_explore)
-        self.displayContinuousStyle(layer_explore, 'Score')
-        expr = "\"BU_NAAM\"='{}'".format(subburbe)
-        feat = uf.getFeaturesByExpression(layer_explore, expr)
-        print(feat[0])
-        self.showresults(feat)
+        if not self.EnterPostalCode == "":
+            self.pref[0] = self.SliderPeople.value()
+            self.pref[1] = self.SliderChild.value()
+            self.pref[2] = self.SliderAccess.value()
+            self.pref[3] = self.SliderAfford.value()
+            self.TabPreferences.setEnabled(False)
+            self.TabMetrics.setEnabled(True)
+            self.Tabs.setCurrentIndex(2)
 
     def determineScore(self, layer):
         res = False
@@ -310,61 +264,45 @@ class MyPluginDockWidget(QtGui.QDockWidget, FORM_CLASS):
             if caps & QgsVectorDataProvider.AddAttributes:
                 layer.startEditing()
                 for feature in layer.getFeatures():
-                    feature['B1'] = feature['B1'] * ((feature['25_44_Norm'] + feature['HH_MK_NORM']) / 2)
-                    if feature['KDV_NORM'] != 0.0 and feature['BSO_NORM'] != 0.0:
-                        feature['B2'] = feature['B2'] * (
-                                (((1 - feature['KDV_NORM'])) + ((1 - feature['BSO_NORM']))) / 2)
-                    else:
-                        if feature['KDV_NORM'] != 0.0 and feature['BSO_NORM'] == 0.0:
-                            feature['B2'] = feature['B2'] * (((1 - feature['KDV_NORM'])))
-                        else:
-                            if feature['KDV_NORM'] == 0 and feature['BSO_NORM'] != 0.0:
-                                feature['B2'] = feature['B2'] * (((1 - feature['BSO_NORM'])))
-                            else:
-                                feature['B2'] = 0
-
-                    if feature['TREIN_NORM'] != 0:
-                        feature['B3'] = feature['B3'] * (1 - feature['TREIN_NORM'])
-                    else:
-                        feature['B3'] = 0
-
-                    if feature['WOZ_NORM'] != 0:
-                        feature['B4'] = feature['B4'] * (1 - feature['WOZ_NORM'])
-                    else:
-                        feature['B4'] = 0
-
-                    feature['Score'] = feature['B1'] + feature['B2'] + feature['B3'] + feature['B4']
+                    feature['B1'] = feature['B1'] * feature['25_44_Norm']
+                    feature['B2'] = feature['B2'] * ((feature['KDV_NORM'])+(feature['BSO_NORM']))
+                    feature['B3'] = feature['B3'] * (feature['TREIN_NORM'])
+                    feature['B4'] = feature['B4'] * (feature['WOZ_NORM'])
+                    feature['Score'] = feature['B1']+ feature['B2'] + feature['B3'] + feature['B4']
                     layer.updateFeature(feature)
 
                 layer.commitChanges()
             res = True
         return res
 
-    def display_point(self, point):
+
+    def display_point(self,point):
 
         coords = "Map Coordinates: {:.4f}, {:.4f}".format(point.x(), point.y())
 
-        print
-        coords
+        print coords
+<<<<<<< HEAD
         closestFeatureId = -1
+=======
+        shortestDistance = float("inf")
+        closestFeatureId = 0
+>>>>>>> b5b21b52cc68afb3c1cfd84bb738aa3660206510
 
         layer = uf.getLegendLayerByName(self.iface, "Rotterdam_Selection")
 
         if str(layer) != "None":
-            pPnt = QgsGeometry.fromPoint(point)
+            pPnt = QgsGeometry.fromPoint(QgsPoint(point.x(), point.y()))
             feats = [feat for feat in layer.getFeatures()]
-            print
-            feats[1]
+            print feats[1]
             for feat in feats:
                 if feat.geometry().contains(pPnt):
                     closestFeatureId = feat.id()
                     break
 
             testlength = str(closestFeatureId)
-            print
-            testlength
+            print testlength
 
-        if testlength != -1:
+        if testlength != -1 :
             fid = closestFeatureId
             iterator = layer.getFeatures(QgsFeatureRequest().setFilterFid(fid))
             featuree = next(iterator)
@@ -374,10 +312,13 @@ class MyPluginDockWidget(QtGui.QDockWidget, FORM_CLASS):
         else:
             parishName = None
 
-        print
-        parishName
+        print parishName
 
-    #######
+
+
+
+
+#######
 #   Data functions
 #######
 
